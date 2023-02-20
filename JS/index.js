a0a0da98666d3d5e1edcda96c0ff68b81cd47c9d
NickoMaill <nicomaillols@gmail.com>
2023-02-20 05:46:04 +0100

    style audio player

--- JS/index.js
+++ JS/index.js
@@ -18,11 +18,21 @@ const muteControl1 = document.querySelector('#muteControl1');
 const muteControl2 = document.querySelector('#muteControl2');
 const muteControl3 = document.querySelector('#muteControl3');
 
+const volume1Container = document.querySelector("#volume1Container");
+const volume2Container = document.querySelector("#volume2Container");
+const volume3Container = document.querySelector("#volume3Container");
+
 const audioPlayer = (control, time, audioDuration, audioPath, whichAudio, volume, mute) => {
     const wavesurfer = WaveSurfer.create({
         container: `#waveform${whichAudio}`,
         waveColor: '#4D4D4D',
         progressColor: '#ff6b46',
+        barWidth: 2,
+        barRadius: 3,
+        cursorWidth: 0,
+        barGap: 3,
+        barHeight: 0.4,
+        maxCanvasWidth: 1000,
     });
 
     wavesurfer.load(audioPath);
@@ -41,6 +51,10 @@ const audioPlayer = (control, time, audioDuration, audioPath, whichAudio, volume
             wavesurfer.setMute(true);
             mute.childNodes[1].src = '../assets/svg/mute.svg';
         } else {
+            if (Number(volume.value) === 0) {   
+                volume.value = 0.1
+            }
+
             wavesurfer.setMute(false);
             mute.childNodes[1].src = '../assets/svg/volume.svg';
         }
@@ -74,6 +88,18 @@ const duration = (time) => {
     return `0${minutes} : ${seconds < 10 ? '0' + Math.floor(seconds) : Math.floor(seconds)}`;
 };
 
+const setSlideVolumeDisplay = (container, slider) => {
+    container.addEventListener('mouseover', () => {
+        slider.classList.remove('d-none')
+    });
+    container.addEventListener('mouseout', () => {
+        slider.classList.add('d-none')
+    });
+}
 audioPlayer(audioControl1, audio1Time, audio1Duration, '../audio/Please-Please-Me.mp3', 1, volume1, muteControl1);
 audioPlayer(audioControl2, audio2Time, audio2Duration, '../audio/Rolling-In-The-Deep.mp3', 2, volume2, muteControl2);
 audioPlayer(audioControl3, audio3Time, audio3Duration, '../audio/Walking-On-Sunshine.mp3', 3, volume3, muteControl3);
+
+setSlideVolumeDisplay(volume1Container, volume1);
+setSlideVolumeDisplay(volume2Container, volume2);
+setSlideVolumeDisplay(volume3Container, volume3);
